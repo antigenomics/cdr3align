a0a902147990ca684785ef9d1573d522feebcf86
mikessh <mikhail.shugay@gmail.com>
2016-07-14 11:16:11 +0200

    On the road to CLI, refactoring

--- src/main/groovy/com/antigenomics/cdr3align/TrainAlignerScoring.groovy
+++ src/main/groovy/com/antigenomics/cdr3align/TrainAlignerScoring.groovy
@@ -14,11 +14,50 @@ import org.moeaframework.util.progress.ProgressListener
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicInteger
 
-// requires a pre-built database
-// load records
 def sout = {
     println "[CDRALING ${new Date().toString()}] $it"
 }
+
+def DEFAULT_CONF_THRESHOLD = "1", DEFAULT_SPECIES = "HomoSapiens",
+    DEFAULT_GENES = "TRA,TRB", DEFAULT_MIN_CDR3_PER_AG = "2",
+    DEFAULT_SCOPE = "5,2,2,7", DEFAULT_PATH = "vdjdb.slim.txt",
+    DEFAULT_MOEA_POP_SIZE = "150", DEFAULT_MOEA_GEN = "1000"
+
+
+def cli = new CliBuilder(usage: "TrainAlignerScoring [options] " +
+        "[sample1 sample2 ... if not -m] output_prefix")
+cli.h("display help message")
+cli._(longOpt: "vdjdb-conf-threshold", argName: "0-3", args: 1,
+        "VDJdb confidence score threshold. [default=$DEFAULT_CONF_THRESHOLD]")
+cli._(longOpt: "species", argName: "HomoSapiens,...", args: 1,
+        "Species list. [default=$DEFAULT_SPECIES]")
+cli._(longOpt: "genes", argName: "TRA,...", args: 1,
+        "Gene list. [default=$DEFAULT_GENES]")
+cli._(longOpt: "min-cdr3-per-ag", argName: "1+", args: 1,
+        "Minimal number of unique CDR3 sequences per antigen. [default=$DEFAULT_MIN_CDR3_PER_AG]")
+cli._(longOpt: "search-scope", argName: "s,i,d,t", args: 1,
+        "Overrides CDR3 sequence initial search parameters: allowed number of substitutions (s), insertions (i), " +
+                "deletions (d) and total number of mutations. [default=$DEFAULT_SCOPE]")
+cli._(longOpt: "vdjdb-slim-path", argName: "path/to/vdjdb.slim.txt", args: 1,
+        "Path to vdjdb table in slim format. [default=$DEFAULT_PATH]")
+cli._(longOpt: "moea-pop-size", argName: "50-250", args: 1,
+        "MOEA population size. [default=$DEFAULT_MOEA_POP_SIZE]")
+cli._(longOpt: "moea-gen", argName: "100+", args: 1,
+        "Number of MOEA generations to urn. [default=$DEFAULT_MOEA_GEN]")
+
+def opt = cli.parse(args)
+
+if (opt == null) {
+    System.exit(2)
+}
+
+if (opt.h) {
+    cli.usage()
+    System.exit(2)
+}
+
+// requires a pre-built database
+// load records
 
 sout "Loading database"
 
